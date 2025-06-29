bb2ffd8f416709de93f8edfd77a43799cbb96113
nanami <nanami@localhost>
2025-06-30 00:26:57 +0900

    Java31〜33完了いたしました

---
+++ HelloWorld_YamaneNanami/src/curriculum33_2/Car.java
@@ -0,0 +1,7 @@
+package curriculum33_2;
+
+import curriculum33.Vehicle;
+
+public class Car extends Vehicle{
+	
+}
---
+++ HelloWorld_YamaneNanami/src/curriculum33_3/Bicycle.java
@@ -0,0 +1,7 @@
+package curriculum33_3;
+
+import curriculum33.Vehicle;
+
+public class Bicycle extends Vehicle{
+	
+}
